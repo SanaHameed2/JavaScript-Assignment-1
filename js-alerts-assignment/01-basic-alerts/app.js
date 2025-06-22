b99a2db33a63db03de04ed36f2f46aecbe13c04e
SanaHameed2 <sana.faiza2@gmail.com>
2025-06-22 19:27:28 +0500

    first commit

---
+++ js-alerts-assignment/01-basic-alerts/app.js
@@ -0,0 +1,8 @@
+// Task 1 : Greet your website visitors
+alert("Hey There! 👋 Welcome to My Website 🚀 Lets Explore Some JavaScript Logics");
+
+// Task 2 ; show an error message
+alert("❌ Error! Please Enter a Valid Password");
+
+// Task 3: Message using a line break
+alert("Hello World! 🌍\nWelcome to My Website! 🚀");
---
+++ js-alerts-assignment/02-sequence-alerts/app.js
@@ -0,0 +1,9 @@
+// Task 4: Two alerts in sequence
+
+alert("This is the first alert.🌟");
+alert("This is the second alert.🌟");
+// The first alert will display a message, and once the user clicks "OK", the second alert will appear.
+
+// Task 5: Message via browser console
+console.log("This is a message in the browser console.💬");
+// The message will be logged in the browser's console, which can be viewed by opening the developer tools (usually F12 or right-click and select "Inspect").
